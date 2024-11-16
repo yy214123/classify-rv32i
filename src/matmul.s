61b10150d79e68f05a07f9b357c97941152e0af6
yy214123 <yy214123@gmail.com>
2024-11-16 23:47:28 +0800

    Implement inner and outer loops in matmul.s
    Complete the missing inner and outer loops in matmul.s to enable
    matrix multiplication. Leverage the Strided Dot Product Calculator
    and use strides to adjust matrix indices for computation.

--- src/matmul.s
+++ src/matmul.s
@@ -116,6 +116,23 @@ inner_loop_start:
     
 inner_loop_end:
     # TODO: Add your own implementation
+    addi s0, s0, 1
+    # move to first element of the next row
+    slli t0, a2, 2			
+    add s3, s3, t0			
+    			
+    j outer_loop_start
+    
+outer_loop_end:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
+	jr ra
 
 error:
     li a0, 38
