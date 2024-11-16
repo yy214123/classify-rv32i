cecbee4413d149fe70917b289c46cacd6cb91165
yy214123 <yy214123@gmail.com>
2024-11-16 18:28:39 +0800

    Implement dot function
    Add a function `dot` to compute the dot product of two integer arrays
    with configurable strides. The function calculates the sum of products
    from corresponding elements of the input arrays, accounting for positive
    and negative values.

--- src/dot.s
+++ src/dot.s
@@ -37,7 +37,47 @@ dot:
 loop_start:
     bge t1, a2, loop_end
     # TODO: Add your own implementation
-
+    lw t2, 0(a0)
+    lw t3, 0(a1)
+    
+    srli t4, t2, 31
+    srli t5, t3, 31
+    bne t4, t5, decrement_cumulatively
+accumulate:
+    srai t4, t2, 31
+    xor t2 ,t2 ,t4
+    sub t2, t2, t4
+    
+    srai t5, t3, 31
+    xor t3 ,t3 ,t5
+    sub t3, t3, t5
+    
+    add t0,t0,t2
+    addi t3, t3, -1
+    bne t3, zero, accumulate
+    j next
+decrement_cumulatively:
+    srai t4, t2, 31
+    xor t2 ,t2 ,t4
+    sub t2, t2, t4
+    
+    srai t5, t3, 31
+    xor t3 ,t3 ,t5
+    sub t3, t3, t5
+    
+    sub t0,t0,t2
+    addi t3, t3, -1
+    bne t3, zero, decrement_cumulatively
+    j next
+next:    
+    slli t3, a3, 2
+    add a0, a0, t3
+    slli t3, a4, 2
+    add a1, a1, t3
+    
+    addi t1, t1, 1
+    j loop_start
+    
 loop_end:
     mv a0, t0
     jr ra
