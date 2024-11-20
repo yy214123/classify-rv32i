a128114025671898d6341636d0a9c07e19780940
yy214123 <yy214123@gmail.com>
2024-11-20 20:14:48 +0800

    Optimize dot.s
    Enhance the dot.s implementation by checking if the multiplier is a
    power of 2. When true, leverage bitwise operations for multiplication
    to improve performance. The multiplier's log value determines the
    number of left shifts for the multiplicand.
    
    The log calculation is implemented using Harley's algorithm for
    efficient bit-level operations.

--- src/dot.s
+++ src/dot.s
@@ -1,4 +1,7 @@
 .globl dot
+.data
+Table:
+    .byte 0xFF, 0, 0xFF, 15, 0xFF, 1, 28, 0xFF,16, 0xFF, 0xFF, 0xFF, 2, 21, 29, 0xFF,0xFF, 0xFF, 19, 17, 10, 0xFF, 12, 0xFF,0xFF, 3, 0xFF, 6, 0xFF, 22, 30, 0xFF,14, 0xFF, 27, 0xFF, 0xFF, 0xFF, 20, 0xFF,18, 9, 11, 0xFF, 5, 0xFF, 0xFF, 13, 26, 0xFF, 0xFF, 8, 0xFF, 4, 0xFF, 25, 0xFF, 7, 24, 0xFF, 23, 0xFF, 31, 0xF
 
 .text
 # =======================================================
@@ -37,14 +40,26 @@ dot:
 loop_start:
     bge t1, a2, loop_end
     # TODO: Add your own implementation
+    
+    # If 0 appears, jump to the next element
     lw t2, 0(a0)
     beq t2, zero, next
     lw t3, 0(a1)
     beq t3, zero, next
     
+    # power of 2
+    addi t4, t2, -1
+    and t4, t2, t4
+    beq t4, zero, call_power_of_2_t2
+    
+    addi t4, t3, -1
+    and t4, t3, t4
+    beq t4, zero, call_power_of_2_t3
+    
     srli t4, t2, 31
     srli t5, t3, 31
     bne t4, t5, decrement_cumulatively
+    
 accumulate:
     srai t4, t2, 31
     xor t2 ,t2 ,t4
@@ -58,6 +73,7 @@ accumulate:
     addi t3, t3, -1
     bne t3, zero, accumulate
     j next
+    
 decrement_cumulatively:
     srai t4, t2, 31
     xor t2 ,t2 ,t4
@@ -71,6 +87,46 @@ decrement_cumulatively:
     addi t3, t3, -1
     bne t3, zero, decrement_cumulatively
     j next
+    
+call_power_of_2_t2:
+    mv   a5, t2
+    mv   a6, t3     
+    j  power_of_2_bitwise
+
+call_power_of_2_t3:
+    mv   a5, t3
+    mv   a6, t2     
+    j  power_of_2_bitwise    
+
+power_of_2_bitwise:
+    # log a5 (Harley's algorithm)
+    srli t4, a5, 1    
+    or   a5, a5, t4
+    srli t4, a5, 2    
+    or   a5, a5, t4   
+    srli t4, a5, 4    
+    or   a5, a5, t4   
+    srli t4, a5, 8    
+    or   a5, a5, t4   
+    srli t4, a5, 16   
+    or   a5, a5, t4
+    slli t4, a5, 3    
+    sub  a5, t4, a5   
+    slli t4, a5, 8    
+    sub  a5, t4, a5   
+    slli t4, a5, 8    
+    sub  a5, t4, a5   
+    slli t4, a5, 8    
+    sub  a5, t4, a5
+    srli t4, a5, 26     
+    la   t5, Table    
+    add  t5, t5, t4   
+    lb  a5, 0(t5)
+
+    sll  a6, a6, a5
+    add t0, t0, a6
+    j next
+    
 next:    
     slli t3, a3, 2
     add a0, a0, t3
